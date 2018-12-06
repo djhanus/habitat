6d0f5a4ccd789fa641f7d11e3a51a5d50d587458
djhanus <davidjhanus@gmail.com>
2018-12-06 13:54:58 -0500

    standard page

--- page-standard.php
+++ page-standard.php
@@ -1,11 +1,22 @@
 <?php get_header(); /* Template Name: Standard Page */ ?>
 
   <div class="container">
+
+  	<div id="page-standard">
+
+  		<div class="featured-wrapper">
+
+  			<h1>FEATURED TITLE</h1>
+
+  		</div>
    
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
         the_content();
           endwhile; else: ?>
       <?php endif; ?>
+
+  	</div>
+
 
   </div>
 <?php get_footer(); ?>
