35a8112a289a12e36f59b0fffb6eeef8170fb18b
djhanus <davidjhanus@gmail.com>
2018-12-13 13:42:34 -0500

    homepage template

--- page-home.php
+++ page-home.php
@@ -7,23 +7,72 @@
 
         <h1>Last year you helped us divert 1.1 million pounds to land fills.</h1>
 
-      </div>
+      </div>  <!-- END FEATURE -->
+
 
   		<div class="row one">
 
-        <h2>Become a Homeowner</h2>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+        <div class="content-wrapper">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/home-module-icon.png" />
+          <h2>Become a Homeowner</h2>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+          <div class="orange-line"></div>
+        </div>
 
-  		</div>
+        <div class="content-wrapper">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/home-module-icon.png" />
+          <h2>Become a Homeowner</h2>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+          <div class="orange-line"></div>
+        </div>
+
+        <div class="content-wrapper">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/home-module-icon.png" />
+          <h2>Become a Homeowner</h2>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+          <div class="orange-line"></div>
+        </div>
+
+  		</div> <!-- END ROW -->
 
       <div class="row two">
 
-        <p>Habitat for Humanity sully dolom ipsum fador dolom ipsum fador dolom ipsum fador dolom ipsum fador </p>
+        <div class="content-wrapper">
+          <p>Habitat for Humanity sully dolom ipsum fador dolom ipsum fador dolom ipsum fador dolom ipsum fador fador dolom ipsum fador dolom ipsum fador fador dolom ipsum fador dolom ipsum fador fador dolom ipsum fador dolom ipsum fador fador dolom ipsum fador dolom ipsum fador.</p>
+
+          <p class="second">The only ipsum fador dolom ipsum fador dolom ipsum fador dolom ipsum fador.</p>
+
+        </div>
+
+      </div> <!-- END ROW -->
+
+      <div class="row donate-btn">
+
+          <a href="#" target="_blank">DONATE TODAY</a>
+
+      </div>
+
+      <div class="families-section">
+
+        <p> families output </p>
+
+      </div>
+
+      <div class="row families-btn">
+
+          <a href="#" target="_blank">MEET ALL OF OUR FAMILIES</a>
 
       </div>
 
 
-  	</div>
+
+      <div class="sponsors">
+
+        <p> sponsors section </p>
+        
+      </div>
+
+  	</div> <!-- END HOME PAGE -->
 
 
 <?php get_footer(); ?>
