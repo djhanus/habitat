e0da6f5d9742a63c4ee4a0b3f4b80aa0398b400e
Adam Butcher <adabutch1@gmail.com>
2019-01-05 21:54:26 -0500

    apply page

---
+++ page-apply.php
@@ -0,0 +1,173 @@
+<?php get_header(); /* Template Name: Apply Page */ ?>
+  <div id="page-apply">
+
+    <div class="page-banner"
+         style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/page-banner-placeholder.jpg')">
+      <div class="title">Apply</div>
+    </div><!-- /.page-banner -->
+
+    <div class="section-one">
+      <div>
+        <p>Habitat for Humanity believes that all hard-working individuals and families should have access to the benefits and stability of homeownership. In this belief, we are committed to partnering with families to build simple, decent, and affordable housing.</p>
+
+        <p>The low cost of our homes is made possible by the exceptional support of our community. Building costs remain low thanks to impressive donations and generous volunteerism. These factors make it possible to sell our homes to accepted families with a 0% interest loan and at no profit.</p>
+      </div>
+
+      <div>two</div>
+      <div>three</div>
+      <div>
+        <p>If you or someone you know is interested in applying to be a homeowner through Habitat for Humanity, please consider our three criteria:</p>
+        <ol>
+          <li>Need for Housing</li>
+          <li>Ability to Pay</li>
+          <li>Willingness to Partner</li>
+        </ol>
+      </div>
+    </div><!-- /.section-one -->
+
+    <div class="section-two">
+      <div class="contained">
+        <h1>Overview of the Process for Buying a Habitat Home</h1>
+
+        <div>
+          <p>After determining that you meet our three criteria, please attend one of our homeowner information meetings. Applications are available at the Habitat Office and at Homeowner Information Meetings. We will be accepting applications July 7 – 19.</p>
+
+          <p>Our Family Selection Committee with then review your application and hold a home interview to determine your approval for homeownership.</p>
+
+          <p>Once approved, you will be asked to sign a Covenant Agreement and make a modest deposit toward your home.</p>
+
+          <p>You are then eligible to start earning sweat equity. At minimum, each adult family member will be required to complete ten hours per month. Often a build lot offering is based on sweat equity progress.</p>
+
+          <p>Once your family has enough sweat equity accrued, you will be offered a build lot, at which time you will start working on your own home’s progress!</p>
+
+          <p><strong>View our upcoming Homeownership Program Information Meetings.</strong></p>
+        </div>
+
+        <div>
+          <p>
+            We are pledged to the letter and spirit of U.S. policy for the achievement of equal housing opportunity throughout the nation. We encourage and support an affirmative advertising and marketing program in which there are no barriers to obtaining housing because of race, color, religion, sex, handicap, familial status, national origin, age, marital status, sexual orientation, or sources of income.
+          </p>
+        </div>
+      </div>
+    </div><!-- /.section-two -->
+
+    <div class="section-three">
+      <a href="#" title="Test">How to Qualify</a>
+      <a href="#" title="Test">Scholarships</a>
+      <a href="#" title="Test">Homeowner Meetings</a>
+      <a href="#" title="Test">Warranty Request Form</a>
+    </div><!-- /.section-three -->
+
+    <div class="section-four">
+      <h1>
+        <div class="contained">FAQ</div>
+      </h1>
+
+      <div class="contained">
+        <details>
+          <summary><span>Who pays to build each house?</span>
+            <button>
+              <svg width="67px" height="67px" viewBox="0 0 67 67" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                  <defs>
+                      <polyline id="path-1" points="25.9003906 18.4350586 43.2832031 33.4455566 25.9003906 48.3596191"></polyline>
+                  </defs>
+                  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                      <circle id="Oval" stroke="#149CCD" stroke-width="2" fill-rule="nonzero" cx="33.4731445" cy="33.5268555" r="32.4731445"></circle>
+                      <g id="Path-2" stroke-linecap="round" fill-rule="nonzero" stroke="#149CCD">
+                          <use stroke-width="2" xlink:href="#path-1"></use>
+                          <use stroke-width="1" xlink:href="#path-1"></use>
+                      </g>
+                  </g>
+              </svg>
+            </button>
+          </summary>
+          <p>We work with faith communities, businesses, and other organizations and individuals to sponsor the building of a house. Such sponsors contribute a fee that goes towards buying materials for the home and typically sponsoring organizations also volunteer skilled and unskilled workers to build alongside the homeowners on the house they sponsor.</p>
+
+          <p>Once an affiliate has built 20 houses, the monthly mortgage payments from those 20 families are sufficient to build an additional house each year. Thus, as mortgage payments are recycled, homeowners also sponsor Habitat houses. Other funding comes from grants and individual donations.</p>
+        </details>
+
+        <details>
+          <summary><span>Who pays to build each house?</span>
+            <button>
+              <svg width="67px" height="67px" viewBox="0 0 67 67" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                  <defs>
+                      <polyline id="path-1" points="25.9003906 18.4350586 43.2832031 33.4455566 25.9003906 48.3596191"></polyline>
+                  </defs>
+                  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                      <circle id="Oval" stroke="#149CCD" stroke-width="2" fill-rule="nonzero" cx="33.4731445" cy="33.5268555" r="32.4731445"></circle>
+                      <g id="Path-2" stroke-linecap="round" fill-rule="nonzero" stroke="#149CCD">
+                          <use stroke-width="2" xlink:href="#path-1"></use>
+                          <use stroke-width="1" xlink:href="#path-1"></use>
+                      </g>
+                  </g>
+              </svg>
+            </button>
+          </summary>
+          <p>We work with faith communities, businesses, and other organizations and individuals to sponsor the building of a house. Such sponsors contribute a fee that goes towards buying materials for the home and typically sponsoring organizations also volunteer skilled and unskilled workers to build alongside the homeowners on the house they sponsor.</p>
+
+          <p>Once an affiliate has built 20 houses, the monthly mortgage payments from those 20 families are sufficient to build an additional house each year. Thus, as mortgage payments are recycled, homeowners also sponsor Habitat houses. Other funding comes from grants and individual donations.</p>
+        </details>
+
+        <details>
+          <summary><span>Who pays to build each house?</span>
+            <button>
+              <svg width="67px" height="67px" viewBox="0 0 67 67" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                  <defs>
+                      <polyline id="path-1" points="25.9003906 18.4350586 43.2832031 33.4455566 25.9003906 48.3596191"></polyline>
+                  </defs>
+                  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                      <circle id="Oval" stroke="#149CCD" stroke-width="2" fill-rule="nonzero" cx="33.4731445" cy="33.5268555" r="32.4731445"></circle>
+                      <g id="Path-2" stroke-linecap="round" fill-rule="nonzero" stroke="#149CCD">
+                          <use stroke-width="2" xlink:href="#path-1"></use>
+                          <use stroke-width="1" xlink:href="#path-1"></use>
+                      </g>
+                  </g>
+              </svg>
+            </button>
+          </summary>
+          <p>We work with faith communities, businesses, and other organizations and individuals to sponsor the building of a house. Such sponsors contribute a fee that goes towards buying materials for the home and typically sponsoring organizations also volunteer skilled and unskilled workers to build alongside the homeowners on the house they sponsor. Once an affiliate has built 20 houses, the monthly mortgage payments from those 20 families are sufficient to build an additional house each year. Thus, as mortgage payments are recycled, homeowners also sponsor Habitat houses. Other funding comes from grants and individual donations.</p>
+        </details>
+
+        <details>
+          <summary><span>Who pays to build each house?</span>
+            <button>
+              <svg width="67px" height="67px" viewBox="0 0 67 67" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                  <defs>
+                      <polyline id="path-1" points="25.9003906 18.4350586 43.2832031 33.4455566 25.9003906 48.3596191"></polyline>
+                  </defs>
+                  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                      <circle id="Oval" stroke="#149CCD" stroke-width="2" fill-rule="nonzero" cx="33.4731445" cy="33.5268555" r="32.4731445"></circle>
+                      <g id="Path-2" stroke-linecap="round" fill-rule="nonzero" stroke="#149CCD">
+                          <use stroke-width="2" xlink:href="#path-1"></use>
+                          <use stroke-width="1" xlink:href="#path-1"></use>
+                      </g>
+                  </g>
+              </svg>
+            </button>
+          </summary>
+          <p>We work with faith communities, businesses, and other organizations and individuals to sponsor the building of a house. Such sponsors contribute a fee that goes towards buying materials for the home and typically sponsoring organizations also volunteer skilled and unskilled workers to build alongside the homeowners on the house they sponsor. Once an affiliate has built 20 houses, the monthly mortgage payments from those 20 families are sufficient to build an additional house each year. Thus, as mortgage payments are recycled, homeowners also sponsor Habitat houses. Other funding comes from grants and individual donations.</p>
+        </details>
+
+        <details>
+          <summary><span>Who pays to build each house?</span>
+            <button>
+              <svg width="67px" height="67px" viewBox="0 0 67 67" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                  <defs>
+                      <polyline id="path-1" points="25.9003906 18.4350586 43.2832031 33.4455566 25.9003906 48.3596191"></polyline>
+                  </defs>
+                  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                      <circle id="Oval" stroke="#149CCD" stroke-width="2" fill-rule="nonzero" cx="33.4731445" cy="33.5268555" r="32.4731445"></circle>
+                      <g id="Path-2" stroke-linecap="round" fill-rule="nonzero" stroke="#149CCD">
+                          <use stroke-width="2" xlink:href="#path-1"></use>
+                          <use stroke-width="1" xlink:href="#path-1"></use>
+                      </g>
+                  </g>
+              </svg>
+            </button>
+          </summary>
+          <p>We work with faith communities, businesses, and other organizations and individuals to sponsor the building of a house. Such sponsors contribute a fee that goes towards buying materials for the home and typically sponsoring organizations also volunteer skilled and unskilled workers to build alongside the homeowners on the house they sponsor. Once an affiliate has built 20 houses, the monthly mortgage payments from those 20 families are sufficient to build an additional house each year. Thus, as mortgage payments are recycled, homeowners also sponsor Habitat houses. Other funding comes from grants and individual donations.</p>
+        </details>
+      </div>
+    </div><!-- /.section-four -->
+  </div><!-- /#page-apply -->
+<?php get_footer(); ?>
