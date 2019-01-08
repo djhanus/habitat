723fe1c1a2f007a9349cde3e06fa54031a43b855
djhanus <davidjhanus@gmail.com>
2019-01-08 13:09:10 -0500

    working on homepage

--- page-home.php
+++ page-home.php
@@ -1,13 +1,10 @@
 <?php get_header(); /* Template Name: Home Page */ ?>
-
-
   	<div class="page-home">
 
-      <div class="featured-wrapper">
-
-        <h1>Last year you helped us divert 1.1 million pounds to land fills.</h1>
-
-      </div>  <!-- END FEATURE -->
+      <div class="page-banner"
+         style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/page-banner-placeholder.jpg')">
+      <div class="title">Last year you helped us divert 1.1 million pounds to land fills.</div>
+      </div><!-- /.page-banner -->
 
 
   		<div class="row one">
@@ -52,7 +49,7 @@
 
       </div>
 
-      <div class="families-section">
+      <div class="row families-section">
 
         <p> families output </p>
 
@@ -64,11 +61,38 @@
 
       </div>
 
+      <div class="row events-news-section">
+
+        <div class="events-container">
+          <div class="title">Events</div>
+        </div>
+
+        <div class="news-container">
+          <div class="title">News</div>
+        </div>
+
+      </div>
 
 
-      <div class="sponsors">
 
-        <p> sponsors section </p>
+      <div class="sponsors-section">
+
+        <p>Thank you sponsors & supporters </p>
+
+        <ul>
+          <li><img src="https://placeholder.pics/svg/125x125/DEDEDE/555555/LOGO"></li>
+          <li><img src="https://placeholder.pics/svg/125x125/DEDEDE/555555/LOGO"></li>
+          <li><img src="https://placeholder.pics/svg/125x125/DEDEDE/555555/LOGO"></li>
+          <li><img src="https://placeholder.pics/svg/125x125/DEDEDE/555555/LOGO"></li>
+          <li><img src="https://placeholder.pics/svg/125x125/DEDEDE/555555/LOGO"></li>
+          <li><img src="https://placeholder.pics/svg/125x125/DEDEDE/555555/LOGO"></li>
+          <li><img src="https://placeholder.pics/svg/125x125/DEDEDE/555555/LOGO"></li>
+          <li><img src="https://placeholder.pics/svg/125x125/DEDEDE/555555/LOGO"></li>
+          <li><img src="https://placeholder.pics/svg/125x125/DEDEDE/555555/LOGO"></li>
+          <li><img src="https://placeholder.pics/svg/125x125/DEDEDE/555555/LOGO"></li>
+          <li><img src="https://placeholder.pics/svg/125x125/DEDEDE/555555/LOGO"></li>
+          <li><img src="https://placeholder.pics/svg/125x125/DEDEDE/555555/LOGO"></li>
+        </ul>
         
       </div>
 
