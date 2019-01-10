ae947b8b2cbd1e469b12eae2eb80a55be18670af
djhanus <davidjhanus@gmail.com>
2019-01-10 13:17:38 -0500

    footer

--- footer.php
+++ footer.php
@@ -1,27 +1,63 @@
 </main>
   <footer>
+  	<div class="footer-container">
 
-  	<div class="title">Habitat Office</div>
-  	<br />
-	213 E. Kirkwood Avenue<br />
-	Bloomington, IN 47408<br />
-	<br />
-	Call: (812) 331-4069<br />
-	Fax: (812) 336-6022<br />
-	<br />
-	Monday-Friday: 8am-Noon, 1pm-5pm
+  		<div class="column-one">
+	  		<div class="title">Habitat Office</div>
 
+			213 E. Kirkwood Avenue<br />
+			Bloomington, IN 47408<br />
+			<br />
+			Call: (812) 331-4069<br />
+			Fax: (812) 336-6022<br />
+			<br />
+			Monday-Friday: 8am-Noon, 1pm-5pm
 
-	<div class="title">ReStore</div>
-	<br />
-	850 S. Auto Mall Road<br />
-	Bloomington, IN 47401<br />
-	<br />
-	Call: (812) 331-2660<br />
-	<br />
-	Tuesday-Friday: 10:30am-5:30pm<br />
-	Saturday: 9:30am-4:30pm<br />
+			<div class="horizontal-line"></div>
 
+			<div class="title">ReStore</div>
+
+			850 S. Auto Mall Road<br />
+			Bloomington, IN 47401<br />
+			<br />
+			Call: (812) 331-2660<br />
+			<br />
+			Tuesday-Friday: 10:30am-5:30pm<br />
+			Saturday: 9:30am-4:30pm<br />
+  		</div> <!-- END column -->
+
+  		<div class="column-two">
+  			<ul>
+  				<li><a href="#">Menu Item</a></li>
+  				<li><a href="#">Menu Item</a></li>
+  				<li><a href="#">Menu Item</a></li>
+  				<li><a href="#">Menu Item</a></li>
+  				<li><a href="#">Menu Item</a></li>
+			</ul>
+
+			<ul>
+				<li><a href="#">Menu Item</a></li>
+				<li><a href="#">Menu Item</a></li>
+				<li><a href="#">Menu Item</a></li>
+				<li><a href="#">Menu Item</a></li>
+				<li><a href="#">Menu Item</a></li>
+				<li><a href="#">Menu Item</a></li>
+				<li><a href="#">Menu Item</a></li>
+				<li><a href="#">Menu Item</a></li>
+				<li><a href="#">Menu Item</a></li>
+				<li><a href="#">Menu Item</a></li>
+			</ul>
+  		</div> <!-- END column -->
+
+  		 <div class="column-three">
+  		 	<div class="button">
+  		 		<a href="#">DONATE</a>
+  		 		<a href="#">VOLUNTEER</a>
+  		 		<a href="#">EMAILS</a>
+	 		</div>
+  		</div> <!-- END column -->
+
+	</div> <!-- END footer container -->
 
 
   	<div class="copyright">
