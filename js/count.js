f9152e96e0b26a6390f3be320f4a9f52be3c4688
Adam Butcher <adabutch1@gmail.com>
2019-01-26 16:57:41 -0500

    count wip

--- js/count.js
+++ js/count.js
@@ -1,11 +1,31 @@
-$('.count-animation').each(function () {
-  $(this).prop('Counter',0).animate({
-    Counter: $(this).text()
-  }, {
-    duration: 2000,
-    easing: 'swing',
-    step: function (now) {
-      $(this).text(Math.ceil(now));
+
+
+$(function() {
+  let sectOneElm       = $('.section-one');
+  var oTop             = $('.section-one').offset().top - window.innerHeight;
+  console.log(oTop);
+  $(window).scroll(function(){
+    console.log('scrolled');
+    var pTop = $('body').scrollTop();
+    console.log( pTop + ' - ' + oTop );   //just for your debugging
+    if( pTop > oTop ){
+        start_count();
     }
   });
 });
+
+
+function start_count(){
+  let sectOneCountElms = $('.section-one .count-animation');
+  $(sectOneCountElms).each(function () {
+    $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 2000,
+      easing: 'swing',
+      step: function (now) {
+        $(this).text(Math.ceil(now));
+      }
+    });
+  });
+}
