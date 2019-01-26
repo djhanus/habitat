1371deae086e2131ecdc8cdd38badf63bc4ca5a7
Adam Butcher <adabutch1@gmail.com>
2019-01-25 21:34:21 -0500

    toggle details

--- functions.php
+++ functions.php
@@ -8,6 +8,7 @@ function enqueue_parent_styles() {
   wp_enqueue_script('popper-script', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', false, null, true);
   wp_enqueue_script('bootstrap-script', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', false, null, true);
   wp_enqueue_script('count-animation-script',  get_stylesheet_directory_uri() .'/js/count.js', false, null, true);
+  wp_enqueue_script('details-toggle-script',  get_stylesheet_directory_uri() .'/js/details-toggle.js', false, null, true);
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
