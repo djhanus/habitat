9b168e2a64aa45733d6a4ed07e47f940c0affce6
djhanus <davidjhanus@gmail.com>
2019-02-07 14:27:26 -0500

    news roll page

--- page-news.php
+++ page-news.php
@@ -1,18 +1,54 @@
 <?php get_header(); /* Template Name: News */ ?>
 
-  <div class="page-standard">
+  <div id="page-news">
 
-    <div class="page-banner"
-      style="background-image: url('<?php echo get_field('image_hero_banner')?>">
-      <div class="title"><?php the_title(); ?></div>
-    </div><!-- /.page-banner -->
-     
-    <div class="main-container">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
-          the_content();
-            endwhile; else: ?>
-        <?php endif; ?>
-    </div> <!-- END main container -->
+    <!-- FEATURED POST HEADER -->
+    <?php
+
+      $post_object = get_field('featured_post_picker');
+
+      if( $post_object ): 
+
+        // override $post
+        $post = $post_object;
+        setup_postdata( $post ); 
+    ?>
+
+    <div class="featured-post"
+      style="background-image: url('<?php echo get_field('featured_image')?>">
+      <div class="container">
+        <div class="date"><?php echo get_the_date(); ?></div>
+        <div class="title"><?php the_title(); ?></div>
+        <p><?php the_excerpt(); ?></p>
+      </div>
+    </div><!-- /.featured-post -->
+
+      <?php wp_reset_postdata(); ?>
+    <?php endif; ?>
+
+    <!-- MAIN NEWS ROLL -->
+    <div class="news-container">
+      <div class="grid news-wrapper">
+          <?php
+            $news_args = array(
+              'post_type'       => 'post',
+              'order'           => 'DSC',
+              'posts_per_page'  => '6',
+              'post_status'     => 'publish',
+              'category_slug'   => 'news',
+            );
+            $news_query  = new WP_Query( $news_args );
+            if($news_query->have_posts() ) : while ( $news_query->have_posts() ) : $news_query->the_post();
+            ?>
+            <div class="grid-item news">
+              <h2><?php echo get_the_date(); ?></h2>
+              <h1><?php the_title(); ?></h1>
+              <?php the_excerpt(); ?>
+              <a href="<?php echo get_permalink(); ?>">Read More >></a>
+            </div>
+          <?php endwhile; endif; wp_reset_query(); ?>
+        </div> <!-- /.news-wrapper -->
+      </div> <!-- /.news-container -->
   
   </div>
 <?php get_footer(); ?>
