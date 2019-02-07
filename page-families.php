14b86c203c9934cdbcc21b2bd387785e8d885aa5
djhanus <davidjhanus@gmail.com>
2019-02-07 10:28:23 -0500

    latest

---
+++ page-families.php
@@ -0,0 +1,18 @@
+<?php get_header(); /* Template Name: Our Families */ ?>
+
+  <div class="page-standard">
+
+    <div class="page-banner"
+      style="background-image: url('<?php echo get_field('featured_banner')?>">
+      <div class="title"><?php echo get_field('featured_banner_text')?></div>
+    </div><!-- /.page-banner -->
+     
+    <div class="main-container">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+          the_content();
+            endwhile; else: ?>
+        <?php endif; ?>
+    </div> <!-- END main container -->
+  
+  </div>
+<?php get_footer(); ?>
