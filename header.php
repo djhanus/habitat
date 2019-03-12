2f94c69077b2dbbf16537480f3f95cdb046af28c
Adam Butcher <adabutch1@gmail.com>
2019-03-11 20:50:37 -0400

    additional responsive, logo

--- header.php
+++ header.php
@@ -45,15 +45,13 @@
     <div class="container">
       <a href="<?php echo get_home_url(); ?>">
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-horizontal-black.png" class="desktop">
-      </a>
 
-      <a href="<?php echo get_home_url(); ?>">
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-horizontal-white.png" class="responsive">
       </a>
 
-        <div class="main-menu-container">
-          <?php wp_nav_menu( array( 'theme_location' => 'primary-menu' ) ); ?>
-        </div>
+      <div class="main-menu-container">
+        <?php wp_nav_menu( array( 'theme_location' => 'primary-menu' ) ); ?>
+      </div>
     </div>
   </header>
 <main>
