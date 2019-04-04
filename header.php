d7c91d273bba4bfc8c3c292e95df91d14992904f
djhanus <davidjhanus@gmail.com>
2019-04-04 11:36:42 -0400

    added analytics, youtube iframe fix

--- header.php
+++ header.php
@@ -13,6 +13,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php bloginfo('description'); ?>">
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-137694237-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-137694237-1');
+    </script>
+
+
     <?php wp_head(); ?>
   </head>
 
